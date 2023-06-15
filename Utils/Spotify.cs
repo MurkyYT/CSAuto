5fd792ee6f175a0203ef69ec21ad811960ea01bb
MurkyYT <71258936+MurkyYT@users.noreply.github.com>
2023-06-15 13:39:45 +0300

    changed Spotify class type

--- Utils/Spotify.cs
+++ Utils/Spotify.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CSAuto.Utils
 {
-    internal class Spotify
+    public static class Spotify
     {
+       
     }
 }
