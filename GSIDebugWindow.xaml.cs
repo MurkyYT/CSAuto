6fd2afcc4aad5cd88ea20941819cb97f731d2737
MurkyYT <71258936+MurkyYT@users.noreply.github.com>
2023-06-18 13:52:33 +0300

    fixed issues after merging

--- GSIDebugWindow.xaml.cs
+++ GSIDebugWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using CSAuto.Utils;
 namespace CSAuto
 {
