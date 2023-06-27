1a92fe59de03af5b0433c5048305bf2aacde42cc
MurkyYT <71258936+MurkyYT@users.noreply.github.com>
2023-06-27 15:05:39 +0300

    little fix not really neccessery

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -554,7 +554,7 @@ namespace CSAuto
                 }
                 DiscordRpc.UpdatePresence(ref this.presence);
             }
-            else
+            else if(!discordRPCON)
             {
                 DiscordRpc.Shutdown();
             }
