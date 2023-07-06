6e3163381f82b7ba240ed800944366f342b0c2b9
MurkyYT <71258936+MurkyYT@users.noreply.github.com>
2023-07-06 15:35:11 +0300

    getting half of screen not always finds button..

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,7 +37,7 @@ namespace CSAuto
         /// <summary>
         /// Constants
         /// </summary>
-        const string VER = "1.0.6";
+        const string VER = "1.0.7";
         const string PORT = "11523";
         const string TIMEOUT = "5.0";
         const string BUFFER = "0.1";
@@ -840,7 +840,7 @@ namespace CSAuto
                         csgoResolution.X / 2,
                         0),
                         Point.Empty,
-                        new System.Drawing.Size(1, csgoResolution.Y / 2));
+                        new System.Drawing.Size(1, csgoResolution.Y));
                 }
                 if (Properties.Settings.Default.saveDebugFrames)
                 {
