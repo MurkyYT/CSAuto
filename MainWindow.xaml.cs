5b27276ff1a9cf2320092c290a28dc46f204df8e
MurkyYT <shooterkingof@gmail.com>
2023-07-11 23:01:07 +0300

    cleaned mess

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -590,15 +590,6 @@ namespace CSAuto
 
             return (foregroundPid == pid);
         }
-        bool SocketConnected(Socket s)
-        {
-            bool part1 = s.Poll(1000, SelectMode.SelectRead);
-            bool part2 = (s.Available == 0);
-            if (part1 && part2)
-                return false;
-            else
-                return true;
-        }
         private void SendMessageToServer(string message)
         {
             try // Try connecting and send the message bytes  
