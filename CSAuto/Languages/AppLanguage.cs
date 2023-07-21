719040467d98d69d4dbb4b8587c7da0f03f0f086
MurkyYT <shooterkingof@gmail.com>
2023-07-21 19:44:35 +0300

    Changed how languages work in the app

---
+++ CSAuto/Languages/AppLanguage.cs
@@ -0,0 +1,19 @@
+﻿using CSAuto.Languages;
+namespace CSAuto
+{
+    public static class AppLanguage
+    {
+        public static string Get(string category)
+        {
+            switch (Properties.Settings.Default.currentLanguage)
+            {
+                case "language_english":
+                    return English.Get(category);
+                case "language_russian":
+                    return Russian.Get(category);
+                default:
+                    return English.Get(category);
+            }
+        }
+    }
+}
--- CSAuto/Languages/Russian.cs
+++ CSAuto/Languages/Russian.cs
@@ -1,106 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace CSAuto
+namespace CSAuto.Languages
 {
-    public static class AppLanguage
+    class Russian
     {
-        public static string Get(string category)
-        {
-            try
-            {
-                switch (Properties.Settings.Default.currentLanguage)
-                {
-                    case "language_english":
-                        return English[category];
-                    case "language_russian":
-                        return Russian[category];
-                    default:
-                        return English[category];
-                }
-            }
-            catch (KeyNotFoundException)
-            {
-                try
-                {
-                    return English[category];
-                }
-                catch
-                {
-                    return category;
-                }
-            }
-        }
-        static readonly Dictionary<string, string> English = new Dictionary<string, string>()
-        {
-            ["language_english"] = "English",
-            ["language_russian"] = "Russian",
-            ["menu_debug"] = "Debug",
-            ["menu_language"] = "Language",
-            ["menu_mobile"] = "Mobile App",
-            ["menu_exit"] = "Exit",
-            ["menu_automation"] = "Automation",
-            ["menu_options"] = "Options",
-            ["menu_opendebug"] = "Open Debug Window",
-            ["menu_checkforupdates"] = "Check for updates",
-            ["menu_enterip"] = "Enter IP address",
-            ["menu_discordrpc"] = "Discord RPC",
-            ["menu_enabled"] = "Enabled",
-            ["menu_autocheckupdates"] = "Check For Updates On Startup",
-            ["menu_autospotify"] = "Auto Pause/Resume Spotify",
-            ["menu_startup"] = "Start With Windows",
-            ["menu_continuespray"] = "Continue Spraying (Experimental)",
-            ["menu_savedebugframes"] = "Save Frames",
-            ["menu_savedebuglogs"] = "Save Logs",
-            ["menu_autoaccept"] = "Auto Accept Match",
-            ["menu_autobuyarmor"] = "Auto Buy Armor",
-            ["menu_autobuydefuse"] = "Auto Buy Defuse Kit",
-            ["menu_preferarmor"] = "Prefer armor",
-            ["menu_autobuy"] = "Auto Buy",
-            ["menu_discord"] = "Discord",
-            ["menu_autoreload"] = "Auto Reload",
-            ["title_debugwind"] = "CSAuto - Debug Window",
-            ["title_update"] = "Check for updates (CSAuto)",
-            ["title_restartneeded"] = "Restart app",
-            ["title_error"] = "Error",
-            ["inputtitle_mobileip"] = "Mobile Phone IP Address",
-            ["inputtext_mobileip"] = "Enter the IP address you see in the app:",
-            ["msgbox_latestversion"] = "You have the latest version!",
-            ["msgbox_newerversion1"] = "Found newer verison",
-            ["msgbox_newerversion2"] = "would you like to download it?",
-            ["msgbox_restartneeded"] = "You must restart the program to apply these changes",
-            ["error_update"] = "Couldn't check for updates. Try again later",
-            ["error_startup1"] = "An error ocurred",
-            ["error_startup2"] = "Try to download the latest version from github.",
-            ["menu_notifications"] = "Notifications",
-            ["menu_acceptednotification"] = "Accepted match",
-            ["menu_mapnotification"] = "Loaded on map",
-            ["menu_lobbynotification"] = "Loaded in lobby",
-            ["menu_connectednotification"] = "Computer connected",
-            ["menu_crashednotification"] = "Game crashed",
-            ["server_computer"] = "Computer",
-            ["server_online"] = "is online",
-            ["server_loadedmap"] = "Loaded on map",
-            ["server_mode"] = "in mode",
-            ["server_loadedlobby"] = "Loaded in lobby!",
-            ["server_gamecrash"] = "The game crashed!",
-            ["server_acceptmatch"] = "Accepted a match!",
-            ["menu_bombnotification"] = "Bomb information",
-            ["server_timeleft"] = "Bomb seconds left:",
-            ["server_bombexplode"] = "The bomb exploded",
-            ["server_bombdefuse"] = "Bomb has been defused",
-            ["exception_steamnotfound"] = "Couldn't find Steam Path",
-            ["exception_nonetworkadapter"] = "No network adapters with an IPv4 address in the system!",
-            ["exception_csgonotfound"] = "Couldn't find CS:GO directory",
-            ["menu_entersteamkey"] = "Enter Steam Web API Key",
-            ["inputtitle_steamkey"] = "Steam Web API Key",
-            ["inputtext_steamkey"] = "Please enter your Steam Web API Key",
-            ["menu_lobbycount"] = "Show players lobby count",
-        };
-        static readonly Dictionary<string, string> Russian = new Dictionary<string, string>()
+        static Dictionary<string, string> translation = new Dictionary<string, string>()
         {
             ["language_english"] = "Английский",
             ["language_russian"] = "Русский",
@@ -166,5 +70,9 @@ namespace CSAuto
             ["inputtext_steamkey"] = "Пожалуйста, введите свой ключ Steam Web API",
             ["menu_lobbycount"] = "Показать количество игроков в лобби",
         };
+        public static string Get(string category)
+        {
+            if (translation.ContainsKey(category)) return translation[category]; else return category;
+        }
     }
 }
