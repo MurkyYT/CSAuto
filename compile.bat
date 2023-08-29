f7377069d42cfcd7343dce74a407629f71064d57
MurkyYT <shooterkingof@gmail.com>
2023-08-29 20:29:38 +0300

    moved all projects to src

--- compile.bat
+++ compile.bat
@@ -1,41 +1,41 @@
 @echo off
 @echo Cleaning csauto project...
-msbuild CSAuto\CSAuto.csproj /t:Clean /property:Configuration=Release > nul
+msbuild src\CSAuto\CSAuto.csproj /t:Clean /property:Configuration=Release > nul
 @echo Cleaned csauto project
 @echo Compiling csauto project...
-msbuild CSAuto\CSAuto.csproj /property:Configuration=Release > nul
+msbuild src\CSAuto\CSAuto.csproj /property:Configuration=Release > nul
 @echo Compiled csauto project
 ConfuserEx\confuser.cli -n CSAuto.crproj > nul
-echo f | xcopy /s /y APIKeys\bin\Release\Confused\APIKeys.dll CSAuto\bin\Release\APIKeys.dll> nul
+echo f | xcopy /s /y src\APIKeys\bin\Release\Confused\APIKeys.dll src\CSAuto\bin\Release\APIKeys.dll> nul
 @echo confused APIKeys
 @echo ------------------------------------------------------------------
 @echo Cleaning steamapiserver project...
-msbuild SteamAPIServer\SteamAPIServer.csproj /t:Clean /property:Configuration=Release > nul
+msbuild src\SteamAPIServer\SteamAPIServer.csproj /t:Clean /property:Configuration=Release > nul
 @echo Cleaned steamapiserver project
 @echo Compiling steamapiserver project...
-msbuild SteamAPIServer\SteamAPIServer.csproj /property:Configuration=Release > nul
+msbuild src\SteamAPIServer\SteamAPIServer.csproj /property:Configuration=Release > nul
 @echo Compiled steamapiserver project
 @echo ------------------------------------------------------------------
 @echo Cleaning csauto_mobile project...
-msbuild CSAuto_Mobile\CSAuto_Mobile.csproj /t:Clean /property:Configuration=Release > nul
+msbuild src\CSAuto_Mobile\CSAuto_Mobile.csproj /t:Clean /property:Configuration=Release > nul
 @echo Cleaned csauto_mobile project
 @echo Compiling csauto_mobile project...
-msbuild  CSAuto_Mobile\CSAuto_Mobile.csproj /verbosity:normal /t:Rebuild /t:PackageForAndroid /t:SignAndroidPackage /p:Configuration=Release > nul
+msbuild  src\CSAuto_Mobile\CSAuto_Mobile.csproj /verbosity:normal /t:Rebuild /t:PackageForAndroid /t:SignAndroidPackage /p:Configuration=Release > nul
 @echo Compiled csauto_mobile project
 @echo ------------------------------------------------------------------
 set "xprvar="
-for /F "skip=79 delims=" %%i in (CSAuto\MainWindow.xaml.cs) do (if not defined xprvar (set "xprvar=%%i"  & goto compile))
+for /F "skip=79 delims=" %%i in (src\CSAuto\MainWindow.xaml.cs) do (if not defined xprvar (set "xprvar=%%i"  & goto compile))
 :compile
 @echo Compiling the installer...
 ISCC.exe installer.iss /DVERSION_NAME=%xprvar:~35,5%> nul
 @echo Compiled the installer
 @echo ------------------------------------------------------------------
 @echo Copying the apk...
-echo f | xcopy /s /y CSAuto_Mobile\bin\Release\com.murky.csauto_mobile-Signed.apk Output\CSAuto_Android.apk > nul
+echo f | xcopy /s /y src\CSAuto_Mobile\bin\Release\com.murky.csauto_mobile-Signed.apk Output\CSAuto_Android.apk > nul
 @echo Copied csauto_mobile apk
 @echo ------------------------------------------------------------------
 @echo Zipping csauto...
-tar -caf Output\CSAuto_Portable.zip -C CSAuto\bin\Release *.dll *.exe > nul
+tar -caf Output\CSAuto_Portable.zip -C src\CSAuto\bin\Release *.dll *.exe > nul
 @echo Zipped csauto
 @echo ------------------------------------------------------------------
 @echo Everything should be in the Output folder
--- installer.iss
+++ installer.iss
@@ -21,7 +21,7 @@ OutputBaseFilename={#NAME}_Installer
 Compression=lzma
 SolidCompression=yes
 WizardStyle=modern
-SetupIconFile=CSAuto\Icons\main.ico
+SetupIconFile=src\CSAuto\Icons\main.ico
 
 [Languages]
 Name: "english"; MessagesFile: "compiler:Default.isl"
@@ -30,8 +30,8 @@ Name: "english"; MessagesFile: "compiler:Default.isl"
 Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"; Flags: unchecked
 
 [Files]
-Source: "CSAuto\bin\Release\*.exe"; DestDir: "{app}"; Flags: ignoreversion
-Source: "CSAuto\bin\Release\*.dll"; DestDir: "{app}"; Flags: ignoreversion
+Source: "src\CSAuto\bin\Release\*.exe"; DestDir: "{app}"; Flags: ignoreversion
+Source: "src\CSAuto\bin\Release\*.dll"; DestDir: "{app}"; Flags: ignoreversion
 ; NOTE: Don't use "Flags: ignoreversion" on any shared system files
 
 [Icons]
