37d9d6bf9e56db1e79070154e3a990a47539cf5d
MurkyYT <shooterkingof@gmail.com>
2023-08-30 17:25:53 +0300

    updated assembly info

--- src/CSAuto/Properties/AssemblyInfo.cs
+++ src/CSAuto/Properties/AssemblyInfo.cs
@@ -1,18 +1,16 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSAuto")]
+[assembly: AssemblyTitle("CS Automation software")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Murky")]
 [assembly: AssemblyProduct("CSAuto")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(CSAuto.MainWindow.VER)]
