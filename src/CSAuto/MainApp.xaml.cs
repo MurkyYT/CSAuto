b884ecc9817820ed06a3912ac91c1f517ef0bc33
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-01-30 21:11:20 +0200

    chore: 2.1.4

--- src/CSAuto/MainApp.xaml.cs
+++ src/CSAuto/MainApp.xaml.cs
@@ -43,7 +43,7 @@ namespace CSAuto
         #region Constants
         public const string VER = "2.1.4";
         public const string FULL_VER = VER + (DEBUG_REVISION == "" ? "" : " REV "+ DEBUG_REVISION);
-        const string DEBUG_REVISION = "8";
+        const string DEBUG_REVISION = "";
         const string GAME_PROCCES_NAME = "cs2";
         const string GAME_WINDOW_NAME = "Counter-Strike 2";
         const string GAME_CLASS_NAME = "SDL_app";
