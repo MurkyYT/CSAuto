b0759ffe86b2ba7e4e2efb076e6d1d73bc6f54f1
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-02-16 17:43:35 +0200

    chore(code): clean up music class

--- src/CSAuto/CompileInfo.cs
+++ src/CSAuto/CompileInfo.cs
@@ -3,6 +3,6 @@ namespace CSAuto
     public static class CompileInfo
     {
         public static string Date = "16/02/25";
-        public static string Time = "17:40:53";
+        public static string Time = "17:41:59";
     }
 }
