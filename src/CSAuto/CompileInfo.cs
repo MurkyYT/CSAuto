f4b6c84ef0aa86242a3e38c7ecd94a2be6b4d579
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-02-18 21:34:10 +0200

    chore(docs): remove spotify faq

--- src/CSAuto/CompileInfo.cs
+++ src/CSAuto/CompileInfo.cs
@@ -3,6 +3,6 @@ namespace CSAuto
     public static class CompileInfo
     {
         public static string Date = "17/02/25";
-        public static string Time = "22:00:10";
+        public static string Time = "22:03:27";
     }
 }
