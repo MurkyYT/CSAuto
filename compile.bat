34c10e95ea2506a2519855fd0701c25346d7bbea
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-02-22 00:12:03 +0200

    feat(compile): added error checks and coloring

--- compile.bat
+++ compile.bat
@@ -1,34 +1,41 @@
 @echo off
+setlocal
+call :setESC
 title Compiling CSAuto...
 del /f /s /q Output
 rmdir Output
+
 @echo Cleaning steamapiserver project...
 msbuild src\SteamAPIServer\SteamAPIServer.csproj /t:Clean /property:Configuration=Release > nul
 @echo Cleaned steamapiserver project
 @echo Compiling steamapiserver project...
-msbuild src\SteamAPIServer\SteamAPIServer.csproj /property:Configuration=Release
-@echo Compiled steamapiserver project
+msbuild src\SteamAPIServer\SteamAPIServer.csproj /property:Configuration=Release /maxcpucount 
+@if errorlevel 1 echo %ESC%[91mERROR: Error when building SteamAPI%ESC%[0m && pause && exit
+@echo %ESC%[92mSUCCESS: Compiled SteamAPI project%ESC%[0m
 @echo ------------------------------------------------------------------
 @echo Cleaning csauto_mobile project...
 msbuild src\CSAuto_Mobile\CSAuto_Mobile.csproj /t:Clean /property:Configuration=Release > nul
 @echo Cleaned csauto_mobile project
 @echo Compiling csauto_mobile project...
-msbuild  src\CSAuto_Mobile\CSAuto_Mobile.csproj /verbosity:normal /t:Rebuild /t:PackageForAndroid /t:SignAndroidPackage /p:Configuration=Release
-@echo Compiled csauto_mobile project
+msbuild  src\CSAuto_Mobile\CSAuto_Mobile.csproj /verbosity:normal /t:Rebuild /t:PackageForAndroid /t:SignAndroidPackage /p:Configuration=Release /maxcpucount 
+@if errorlevel 1 echo %ESC%[91mERROR: Error when building CSAuto_Mobile%ESC%[0m && pause && exit
+@echo %ESC%[92mSUCCESS: Compiled CSAuto_Mobile project%ESC%[0m
 @echo ------------------------------------------------------------------
 @echo Cleaning updater project...
 msbuild src\Updater\Updater.csproj /t:Clean /property:Configuration=Release > nul
 @echo Cleaned updater project
 @echo Compiling updater project...
-msbuild src\Updater\Updater.csproj /property:Configuration=Release
-@echo Compiled updater project
+msbuild src\Updater\Updater.csproj /property:Configuration=Release /maxcpucount 
+@if errorlevel 1 echo %ESC%[91mERROR: Error when building Updater%ESC%[0m && pause && exit
+@echo %ESC%[92mSUCCESS: Compiled Updater project%ESC%[0m
 @echo ------------------------------------------------------------------
 @echo Cleaning csauto project...
 msbuild src\CSAuto\CSAuto.csproj /t:Clean /property:Configuration=Release > nul
 @echo Cleaned csauto project
 @echo Compiling csauto project...
-msbuild CSAuto.sln /t:CSAuto /p:Configuration="Release"
-@echo Compiled csauto project
+msbuild CSAuto.sln /t:CSAuto /p:Configuration="Release" /maxcpucount 
+@if errorlevel 1 echo %ESC%[91mERROR: Error when building CSAuto%ESC%[0m && pause && exit
+@echo %ESC%[92mSUCCESS: Compiled CSAuto project%ESC%[0m
 @echo ------------------------------------------------------------------
 @echo Compiling the installer...
 ISCC.exe installer.iss
@@ -41,7 +48,13 @@ echo f | xcopy /s /y src\CSAuto_Mobile\bin\Release\net8.0-android34.0\com.murky.
 @echo Zipping csauto...
 tar -caf Output\CSAuto_Portable.zip -C src\CSAuto\bin\Release *.exe resource bin
 @echo Zipped csauto
-@echo ------------------------------------------------------------------
+@echo %ESC%[92m------------------------------------------------------------------
 @echo Everything should be in the Output folder
-@echo ------------------------------------------------------------------
+@echo ------------------------------------------------------------------%ESC%[0m
 pause
+
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)
--- src/CSAuto/CompileInfo.cs
+++ src/CSAuto/CompileInfo.cs
@@ -2,7 +2,7 @@ namespace CSAuto
 {
     public static class CompileInfo
     {
-        public static string Date = "21/02/25";
-        public static string Time = "18:00:39";
+        public static string Date = "22/02/25";
+        public static string Time = "00:11:32";
     }
 }
