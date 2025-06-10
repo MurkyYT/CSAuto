19fe7948b297a18416877ed02055761b241012fb
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-06-10 11:33:32 +0300

    chore: ui :)

--- src/CSAuto/CompileInfo.cs
+++ src/CSAuto/CompileInfo.cs
@@ -3,6 +3,6 @@ namespace CSAuto
     public static class CompileInfo
     {
         public static string Date = "10/06/25";
-        public static string Time = "11:25:28";
+        public static string Time = "11:32:04";
     }
 }
