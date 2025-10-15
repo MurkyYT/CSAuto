0e41e74f4ef0e5e5802076a7105ae2d3c1af6da8
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-10-15 12:03:35 +0300

    chore: 2.2.2

--- src/CSAuto/CompileInfo.cs
+++ src/CSAuto/CompileInfo.cs
@@ -3,6 +3,6 @@ namespace CSAuto
     public static class CompileInfo
     {
         public static string Date = "15/10/25";
-        public static string Time = "11:58:11";
+        public static string Time = "12:02:56";
     }
 }
