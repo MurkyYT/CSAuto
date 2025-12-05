709c78c58c991164c6f86392032adaf3359c3737
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-12-05 18:33:40 +0200

    feat(discord): made preview more accurate

--- src/CSAuto/CompileInfo.cs
+++ src/CSAuto/CompileInfo.cs
@@ -3,7 +3,7 @@ namespace CSAuto
     public static class CompileInfo
     {
         public static string Date = "05/12/25";
-        public static string Time = "18:23:08";
-		public static string GitHash = "5431742";
+        public static string Time = "18:32:36";
+		public static string GitHash = "e88e4aa";
     }
 }
